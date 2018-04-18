1c86b13b097ae4d5b30f75362f4c67ca40fa0fcc
torvik <pless.victor@gmail.com>
2018-04-18 16:26:48 +0000

    complemento

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
 
   include CanCan::ControllerAdditions
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+    end
+  end
+
   
 end
